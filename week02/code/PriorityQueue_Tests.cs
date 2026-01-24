168e153433102823f34d72dd5fa0b1c1791ce812
merkulovvs <18528771+merkulovvs@users.noreply.github.com>
2026-01-25 00:37:57 +0100

    PriorityQueue two bugs fixed.

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 // TODO Problem 2 - Write and run test cases and fix the code to match requirements.
@@ -7,8 +8,9 @@ public class PriorityQueueTests
 {
     [TestMethod]
     // Scenario: Test if the item with the highest priority 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Expected Result: sue
+    // Defect(s) Found: actual result is bob
+    // Fix: in Dequeue function implementation in the condition of the for loop replaced index < _queue.Count - 1 with index < _queue.Count 
     public void TestPriorityQueue_1()
     {
         var priorityQueue = new PriorityQueue();
@@ -17,21 +19,30 @@ public class PriorityQueueTests
         priorityQueue.Enqueue("sue", 10);
 
         var result = priorityQueue.Dequeue();
-        Assert.AreEqual("sue", result);
-        
-      
+        Assert.AreEqual("sue", result);      
 
         //Assert.Fail("Implement the test case and then remove this.");
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Test if between two items with the same priority the item that was added first will be dequeued first.
+    // Expected Result: "jim"
+    // Defect(s) Found: actual result is mik
+    // Fix: In Dequeue function for loop if condition replaced >= to >.
     public void TestPriorityQueue_2()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("bob", 1);
+        priorityQueue.Enqueue("jim", 3);
+        priorityQueue.Enqueue("sue", 1);
+        priorityQueue.Enqueue("mik", 3);
+        
+
+        
+        var result = priorityQueue.Dequeue();
+        Assert.AreEqual("jim", result);      
+        
+        //Assert.Fail("Implement the test case and then remove this.");
     }
 
     // Add more test cases as needed below.
